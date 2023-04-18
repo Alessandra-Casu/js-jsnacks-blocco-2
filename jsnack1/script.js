b90556c8c70b1de29a8a5f36b11385e1b175c097
Alessandra-Casu <alessandracasu919@gmail.com>
2023-04-18 15:20:41 +0200

    add struct jsnack1

---
+++ jsnack1/script.js
@@ -0,0 +1,14 @@
+/*
+Crea un array vuoto.
+Chiedi per 6 volte all’utente di inserire un numero,
+se è dispari inseriscilo nell’array.
+ */
+
+const myArr = [];
+
+
+for(i=0; i<=5; i++){
+    let num = parseInt(prompt('Inserisci un numero'));
+    myArr[i] = num;
+    console.log(num);
+}
