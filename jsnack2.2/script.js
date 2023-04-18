095aef7bdfa0606c9e0197298e397c46bde8603a
Alessandra-Casu <alessandracasu919@gmail.com>
2023-04-18 16:41:52 +0200

    add random

--- jsnack2.2/script.js
+++ jsnack2.2/script.js
@@ -3,20 +3,27 @@ Generatore di “nomi cognomi” casuali: il Grande Gatsby ha una lista di nomi 
  e da queste vuole generare una falsa lista di invitati con nome e cognome.
 
  */
-let nameCasual;
-let lastNameCasual;
 
- const lastNameArr = [ 'Rossi', 'Verdi', 'Gialli', 'Neri', 'Bianchi', 'Grigi'];
+ let numberList = 12;
 
  const nameArr = ['Maria', 'Giuseppe', 'Enrico', 'Caterina',' Alessandro', 'Antonio'];
+ const lastNameArr = [ 'Rossi', 'Verdi', 'Gialli', 'Neri', 'Bianchi', 'Grigi'];
 
  console.log(nameArr);
  console.log(lastNameArr);
 
+ /*
  for(i=0; i<=5; i++){
         for(j=0; j<=5; j++)
         {
             console.log( nameArr[i] + ' ' + lastNameArr[j] );
         }
 
+ }
+ */
+
+ for(i = 0; i < numberList; i++){
+    const nameCasual = Math.floor(Math.random() * nameArr.lenght);
+    const lastNameCasual = Math.floor(Math.random() * lastNameArr.lenght);
+    console.log( nameArr[nameCasual] + ' ' + lastNameArr[lastNameCasual]);
  }
